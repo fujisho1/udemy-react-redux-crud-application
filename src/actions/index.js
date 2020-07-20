c1722c210cafe66aba36b7784fb9d43f66206bda
Shotaro.F <shoutarou_fujie@mj.makita.co.jp>
2020-07-20 09:15:27 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const decrement = () => ({
+    type: DECREMENT
+})
+
+
+
+/*
+    ActionCreater
+*/
