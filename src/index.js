d5869afbaf8c53e85df29ef807b9790ef7a19592
Shotaro.F <shoutarou_fujie@mj.makita.co.jp>
2020-07-21 07:36:54 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+
+
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
